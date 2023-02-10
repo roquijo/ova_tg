631f5cd0bf29727b9ce02fcb3e410c002a26cff4
roquijo <roquijo@hotmail.com>
2023-02-10 10:07:43 -0500

    centrado de la bibliografia

--- src/components/arreglos/BibliographySection.vue
+++ src/components/arreglos/BibliographySection.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-container class="image">
+    <v-container>
         <v-row align="center" justify="center">
             <h2 class="display-2 font-weight-bold mb-4" justify="center" v-t="'bibliographyPage.title'" />
         </v-row>
-        <v-row align="center" class="mb-3">
-            <ul>
-                <li cols="12" md="6" v-for="(item, i) in $t('bibliographyPage.list')" :key="i" class="mb-4">
-                    <h3 class="font-weight-light">{{ item }}</h3>
+        <v-row align="center" class="mb-4" justify="center">
+            <ul class="mb-4" v-for="(item, i) in $t('bibliographyPage.list')" :key="i">
+                <li class="mb-3">
+                    <h3 style="width: 75vw;" class="font-weight-light">{{ item }}</h3>
                 </li>
             </ul>
         </v-row>
@@ -14,19 +14,7 @@
 </template>
 
 <style>
-.video-responsive {
-    overflow: hidden;
-    padding-bottom: 400px;
-    /* Adecua este valor para controlar la altura del video */
-    position: relative;
-    height: 0;
-}
-
-.video-responsive iframe {
-    left: 0;
-    top: 0;
-    height: 100%;
-    width: 100%;
-    position: absolute;
+h3 {
+    word-wrap: break-word;
 }
 </style>
