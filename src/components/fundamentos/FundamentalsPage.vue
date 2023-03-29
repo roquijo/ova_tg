3d49c44b06e2d6f968fef19164e1487bc5f1b2d4
roquijo <roquijo@hotmail.com>
2023-03-29 17:51:53 -0500

    estructura pagina fundamentos

--- src/components/fundamentos/FundamentalsPage.vue
+++ src/components/fundamentos/FundamentalsPage.vue
@@ -7,10 +7,6 @@
             <h2 class="display-2 font-weight-bold mb-4" justify="center" v-t="'fundamentalsPage.title'" />
             <br>
             <h3 class="font-weight-light" justify="center" v-t="'fundamentalsPage.paragraph'" />
-            <v-btn rounded outlined large dark @click="$vuetify.goTo('#fundamentalsSection')" class="mt-5"
-              v-t="'fundamentalsPage.button'">
-              <v-icon class="ml-2">mdi-arrow-down</v-icon>
-            </v-btn>
           </v-col>
           <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
         </v-row>
